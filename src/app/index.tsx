79363cb1095031945bbdaea5855f6b667c45187c
mdabhnx <mdabhnx+github@gmail.com>
2024-03-13 21:55:54 +0600

    linted

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 
 const index = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontFamily: 'Gilroy' }}>Welcome Master</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontFamily: "Gilroy" }}>Welcome Master</Text>
     </View>
   );
 };
