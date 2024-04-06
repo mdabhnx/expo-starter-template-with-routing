2814ae32d7e04e2e9c7b5cbdb47fe8ea6503427c
MD Abid Hossain <mdabhnx+github@gmail.com>
2024-04-06 10:45:59 +0600

    link path modified

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,25 +1,25 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { RootProvider } from "src/CTX/RootContext";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { RootProvider } from '@/context/RootContext';
 
-export { ErrorBoundary } from "expo-router";
+export { ErrorBoundary } from 'expo-router';
 
 export const unstable_settings = {
-  initialRouteName: "index",
+  initialRouteName: 'index',
 };
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
-    DmSans: require("../../assets/fonts/DmSans.ttf"),
-    Gilroy: require("../../assets/fonts/Gilroy-SemiBold.ttf"),
+    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
+    DmSans: require('../../assets/fonts/DmSans.ttf'),
+    Gilroy: require('../../assets/fonts/Gilroy-SemiBold.ttf'),
     ...FontAwesome.font,
   });
 
@@ -46,13 +46,13 @@ function RootLayoutNav() {
       <SafeAreaView style={{ flex: 1 }}>
         <RootProvider>
           <Stack
-            key={"root"}
+            key={'root'}
             screenOptions={{
               headerShown: false,
               gestureEnabled: true,
               statusBarHidden: false,
-              statusBarStyle: "dark",
-              statusBarAnimation: "slide",
+              statusBarStyle: 'dark',
+              statusBarAnimation: 'slide',
             }}
           ></Stack>
         </RootProvider>
